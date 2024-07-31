396a96c4fe3ac3dd2b0d9e40622e6ac6ce46694c
James_Matara <jamesmatara11@gmail.com>
2024-07-31 17:49:08 +0300

    Add a function to enable the drop-down settings menu

---
+++ script.js
@@ -0,0 +1,8 @@
+// Drop Down Menu Start
+let settingsMenu = document.querySelector(".settings-menu");
+
+function settingsMenuToggle() {
+  settingsMenu.classList.toggle("settings-menu-height");
+}
+
+// Drop Down Menu End
